a2838d9fdc28d0f98885bf926299fb6474e31f60
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-19 02:05:00 +0600

    login signup system added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import githubicon from '../../assets/images/icons8-github.svg';
 import fbicon from '../../assets/images/icons8-facebook.svg';
 import './Login.css';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const auth = getAuth(app);
+
 
 const Login = () => {
+  const [errors, setErrors]=useState('')
+  const [users, setUsers]= useState('')
+  const [success, setSuccess]= useState('')
+  //handlelogin form submit btn
+  const handelLogin=(event)=>{
+    event.preventDefault()
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email , password)
+    setErrors('')
+    //firebase login Auth
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user)
+      if(!user.emailVerified){
+        alert(`Your Email is Not Verfyed
+        Please Verify Your Email`)
+        return;
+      }
+      setUsers(user)
+      toast.success('Login sucessfull', {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
+
+      
+      // ...
+    })
+    .catch((error) => {
+      console.log(error)
+      const errorMessage = error.message;
+      setErrors(errorMessage)
+      setSuccess('')
+      setUsers('')
+    });
+
+  }
     return (
         <div className="loginPage">
         <div className="container">
           <div className="register">
             <div className="registerarea">
               <h2>Login</h2>
-              <form>
+              <form onSubmit={handelLogin}>
                   <div className="registerinput">
-                      <input type="email" name="email"  placeholder=" Enter Email" /> <br />
-                      <input type="password" name="passord" placeholder=" Password" /> <br />
+                      <input required type="email" name="email"  placeholder=" Enter Email" /> <br />
+                      <input type="password" name="password" placeholder=" Password" /> <br />
                       <Link className='forgetpassword' to='/forget'>Forget Password?</Link>
-                      <button class="registerBtn"> LogIn</button>
-
+                      <button type='submit' class="registerBtn"> LogIn</button>
+                      <p className='text-danger'>{errors}</p>
+                      <p className='text-success'>{success}</p>
+                      <ToastContainer />
                       <p>Or Using For Login</p>
                       <div className="loginwithSocial">
                     <img className='fbicon' src={fbicon} alt=" Facebook Login" />
@@ -26,6 +80,8 @@ const Login = () => {
                       <div className="singupLink">
                         <Link to='/register'>Create Account</Link>
                       </div>
+                      <p>{users.email}</p>
+                      <p>{users.uid}</p>
                   </div>
               </form>
             </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,31 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const auth = getAuth(app);
 const Register = () => {
-
-  const handelsubmitFrom = (event) => {
+const [regerror, setRegerror]= useState('')
+const [success, setSuccess]= useState('')
+const handelsubmitFrom = (event) => {
+    
     // preventDefult for from reload off
     event.preventDefault();
+    setSuccess('')
+    setRegerror('')
+    
     // const name = event.target.name.value;
 
     //data collect from input field
     const email = event.target.email.value;
     const password = event.target.password.value;
+//password validation
+if(!/(?=.*[A-Z])/.test(password)){
+  setRegerror('Password Start from (A-Z)')
+  return;
+}
+else if(!/(?=.*[a-z].*[0-9])/.test(password)){
+  setRegerror('Password Start from (A-Z & a-z & 0-9 ) Use please')
+  return;
+}
 
     // create user and send firebase
     createUserWithEmailAndPassword(auth, email, password)
-    .then((result) =>{
+    .then(result =>{
       const loggeduser= result.user;
-      console.log(loggeduser)
+      console.log(email , password)
+      console.log(loggeduser.email)
+      event.target.reset()
+      setRegerror('')
+      sendVerification(loggeduser)
+
+      toast.success('account create sucessfull', {
+        position: "bottom-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     })
     .catch(error =>{
-      console.error(error)
+      setRegerror(error.message)
+      setSuccess('')
     })
   };
+  const sendVerification =(loggeduser)=>{
+    sendEmailVerification(loggeduser)
+    .then((result)=>{
+      alert('Please Verify Your Email')
+    })
+  }
   return (
     <div className="loginPage">
       <div className="container">
@@ -36,24 +74,26 @@ const Register = () => {
               <div className="registerinput">
                 <input type="text" name="name" placeholder=" User Name" />{" "}
                 <br />
-                <input
+                <input required
                   type="email"
                   name="email"
                   placeholder=" Enter Email"
                 />{" "}
                 <br />
-                <input
+                <input required
                   type="password"
                   name="password"
                   placeholder=" Password"
                 />{" "}
                 <br />
+                <p className="text-danger">{regerror}</p>
                 <button type="submit" class="registerBtn"> Register</button>
                 <div className="singupLink">
                   <Link to="/login">Login</Link>
                 </div>
               </div>
             </form>
+            <ToastContainer />
           </div>
         </div>
       </div>
